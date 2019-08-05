6a2e70bc30bbd5937055756f57653e5f2dbdc00d
Anthony Dlugosz <53377434+Lol-Key@users.noreply.github.com>
2019-08-05 18:23:32 +0200

    Add files via upload

---
+++ Math/eratosthenes_sieve.cpp
@@ -0,0 +1,21 @@
+//Sieve of Eratosthenes for computing primes in range (1,n] and prime factorization table. Optimized to be faster than standard nloglogn.
+//Time complexity: O(nlog(log(sqrt(n))))
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename t>
+vector<t> eratosthenes_sieve(t n)
+{
+    vector<t> ans;
+    ans.reserve(n+1);
+    for(int i=1;i<=n;i+=2)
+        ans[i]=i;
+    for(int i=2;i<=n;i+=2)
+        ans[i]=2;
+    for(int i=3;i*i<=n;i+=2)
+        for(int j=i;j<=n;j+=i)
+            ans[j]=min(ans[j],i);
+    return ans;
+}
+
